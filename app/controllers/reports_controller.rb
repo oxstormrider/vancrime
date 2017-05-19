c190c0f7d19d5f98a5c376080597ec60680c59d1
Winicius Reis <darkvinny@gmail.com>
2017-05-19 16:30:11 -0300

    Some highchart working

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,14 +4,14 @@ class ReportsController < ApplicationController
   end
 
   def year
-    #@crimes = Crime.select("count(id) as id, offense_id as offense_id, year as year").group(:year, :offense_id).order(:year, :offense_id).paginate(page: params[:page], per_page: 8)
+    @crimes = Crime.select("count(id) as id, offence_id as offence_id, year as year").group(:year, :offence_id).order(:year, :offence_id).paginate(page: params[:page], per_page: 11)
   end
 
   def month
-    #@crimes = Crime.select("count(id) as id, offense_id as offense_id, month as month").group(:month, :offense_id).order(:month, :offense_id).paginate(page: params[:page], per_page: 8)
+    @crimes = Crime.select("count(id) as id, offence_id as offence_id, month as month").group(:month, :offence_id).order(:month, :offence_id).paginate(page: params[:page], per_page: 11)
   end
 
   def year_month
-    #@crimes = Crime.select("count(id) as id, offense_id as offense_id, month as month, year as year").group(:year, :month, :offense_id).order(:year, :month, :offense_id).paginate(page: params[:page], per_page: 96)
+    @crimes = Crime.select("count(id) as id, offence_id as offence_id, month as month, year as year").group(:year, :month, :offence_id).order(:year, :month, :offence_id).paginate(page: params[:page], per_page: 132)
   end
 end
